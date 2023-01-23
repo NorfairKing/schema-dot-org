970e7db11a4b2998e4b3774faf81282eb7024b2c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-01-23 01:05:52 +0100

    An example rendering

--- schema-dot-org/src/SchemaDotOrg/Schema.hs
+++ schema-dot-org/src/SchemaDotOrg/Schema.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PolyKinds #-}
@@ -12,10 +13,12 @@ module SchemaDotOrg.Schema where
 
 import Data.Aeson hiding (Options)
 import qualified Data.Aeson.Key as Key
+import qualified Data.Aeson.KeyMap as KeyMap
 import qualified Data.Aeson.Types as JSON
 import Data.Kind
 import Data.Proxy
 import Data.Text (Text)
+import qualified Data.Text as T
 
 -- Imagine this hyrarchy
 --
@@ -24,7 +27,7 @@ import Data.Text (Text)
 --   -> C -> D
 data A
 
-classA :: Class A '[B, C]
+classA :: Class A '[B, C, D]
 classA = Class "A"
 
 data B
@@ -45,6 +48,14 @@ classD = Class "D"
 propertyDName :: Property D '[Text]
 propertyDName = Property "name"
 
+data E
+
+classE :: Class E '[]
+classE = Class "E"
+
+propertyEAddress :: Property E '[Text]
+propertyEAddress = Property "address"
+
 data Class clazz superClasses = Class Text
 
 data Property clazz expectedTypes = Property Text
@@ -59,18 +70,99 @@ class ParseableOptions expectedTypes where
 instance ParseableOptions '[] where
   parseOptions = UnexpectedOption
 
-instance (FromJSON firstExpectedType, ParseableOptions otherExpectedTypes) => ParseableOptions (firstExpectedType ': otherExpectedTypes) where
+instance
+  (FromJSON firstExpectedType, ParseableOptions otherExpectedTypes) =>
+  ParseableOptions (firstExpectedType ': otherExpectedTypes)
+  where
   parseOptions v = ExpectedOption (JSON.parseEither parseJSON v) (parseOptions v)
 
-parseProperty ::
-  (Inherits (clazz ': superClasses) propertyClass, ParseableOptions expectedTypes) =>
-  Class clazz superClasses ->
+lookupProperty ::
+  (Inherits classes propertyClass, ParseableOptions expectedTypes) =>
   Property propertyClass expectedTypes ->
-  (JSON.Object -> JSON.Parser (Maybe (Options expectedTypes)))
-parseProperty (Class _) (Property propertyName) o = do
-  mValue <- o .:? Key.fromText propertyName
-  pure $ parseOptions <$> mValue
+  ParserOf classes (Maybe (Options expectedTypes))
+lookupProperty (Property propertyName) = ParserOf $ \o ->
+  case KeyMap.lookup (Key.fromText propertyName) o of
+    Nothing -> pure Nothing
+    Just v -> pure $ Just $ parseOptions v
+
+requireProperty ::
+  (Inherits classes propertyClass, ParseableOptions expectedTypes) =>
+  Property propertyClass expectedTypes ->
+  ParserOf classes (Options expectedTypes)
+requireProperty property@(Property propertyName) = do
+  mProperty <- lookupProperty property
+  case mProperty of
+    Nothing -> fail $ unwords ["Property not found: ", show propertyName]
+    Just options -> pure options
 
 class Inherits classes clazz
 
-instance Inherits '[clazz] clazz
+instance {-# OVERLAPS #-} Inherits (clazz ': otherClasses) clazz
+
+instance Inherits otherClasses clazz => Inherits (otherClass ': otherClasses) clazz
+
+newtype ParserOf classes a = ParserOf {unParser :: JSON.Object -> Either String a}
+
+instance Functor (ParserOf classes) where
+  fmap f (ParserOf func) = ParserOf $ \o ->
+    f <$> func o
+
+instance Applicative (ParserOf classes) where
+  pure a = ParserOf $ \_ -> pure a
+  (ParserOf ff) <*> (ParserOf fa) = ParserOf $ \o -> do
+    f <- ff o
+    a <- fa o
+    pure (f a)
+
+instance Monad (ParserOf classes) where
+  (ParserOf fa) >>= func = ParserOf $ \o -> do
+    a <- fa o
+    let (ParserOf fb) = func a
+    fb o
+
+instance MonadFail (ParserOf classes) where
+  fail err = ParserOf $ \_ -> Left err
+
+parseClass :: Class clazz superClasses -> ParserOf (clazz : superClasses) a -> JSON.Value -> Either String a
+parseClass (Class className) (ParserOf parseFunc) value =
+  JSON.parseEither (withObject (T.unpack className) (either fail pure . parseFunc)) value
+
+newtype RenderOf classes = RenderOf {unRenderOf :: JSON.Object}
+  deriving (Semigroup, Monoid)
+
+renderClass :: Class clazz superClasses -> RenderOf (clazz : superClasses) -> JSON.Value
+renderClass (Class _) (RenderOf render) = Object render
+
+class IsExpectedType expectedTypes actualType
+
+instance {-# OVERLAPS #-} IsExpectedType (actualType ': otherTypes) actualType
+
+instance IsExpectedType otherTypes actualType => IsExpectedType (otherType ': otherTypes) actualType
+
+renderProperty ::
+  ( Inherits classes propertyClass,
+    IsExpectedType expectedTypes actualType,
+    ToJSON actualType
+  ) =>
+  Property propertyClass expectedTypes ->
+  actualType ->
+  RenderOf classes
+renderProperty (Property propertyName) actualValue =
+  RenderOf $ KeyMap.singleton (Key.fromText propertyName) (toJSON actualValue)
+
+exampleValue :: JSON.Value
+exampleValue =
+  object
+    [ ("@type", "A"),
+      ("name", "foobar")
+    ]
+
+exampleNameParsing :: Either String Text
+exampleNameParsing = flip (parseClass classA) exampleValue $ do
+  ExpectedOption errOrResult (UnexpectedOption _) <- requireProperty propertyDName
+  case errOrResult of
+    Left err -> fail err
+    Right result -> pure result
+
+exampleARendering :: JSON.Value
+exampleARendering = renderClass classA (renderProperty propertyDName ("quux" :: Text))
